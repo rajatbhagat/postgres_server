2c5f0c7cd9acf1f94e32898b44eae9581516c3e9
rajatbhagat <rajat.bhagat@gmail.com>
2021-10-20 10:38:37 -0400

    Adding the sample apis to create and drop database

--- postgres_database/database_blueprint.py
+++ postgres_database/database_blueprint.py
@@ -17,12 +17,13 @@ def create_database():
     database_name = request.args.get("dbname")
     connection = psycopg2.connect("host='localhost' user=postgres password=test")
     database_creation_sql = "create database " + database_name + ";"
+    connection.autocommit = True
     try:
         with connection.cursor() as cursor:
             cursor.execute(database_creation_sql)
             return "Database created Successfully"
     except Exception as e:
-        return "Exception while creating database : " + e
+        return "Exception while creating database : " + e.__str__()
 
 
 @database_blueprint.route("/dropDatabase")
@@ -30,13 +31,14 @@ def drop_database():
     # url/dropDatabase?dbname=<dbname>
     database_name = request.args.get("dbname")
     connection = psycopg2.connect("host='localhost' user=postgres password=test")
+    connection.autocommit = True
     database_deletion_sql = "drop database " + database_name + ";"
     try:
         with connection.cursor() as cursor:
             cursor.execute(database_deletion_sql)
             return "Database dropped Successfully"
     except Exception as e:
-        return "Exception while dropping database : " + e
+        return "Exception while dropping database : " + e.__str__()
 
 
 @database_blueprint.route("/modifySettings")
