e8549071b0fc0c2f2acf0749856cfac833afaf83
Anirudh Srivatsa <arsrivat@Anirudhs-Air.cable.rcn.com>
2021-11-17 10:49:35 -0500

    Checking nova

---
+++ client/_init_.py
@@ -0,0 +1,13 @@
+import csv
+import novaclient
+header = ['VM', 'IsAlive', 'Space']
+import psycopg2
+from psycopg2.extras import LogicalReplicationConnection
+
+with open('central_repository.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+for instance in novaclient.servers.list():
+    print(instance.name)
+    print(instance.addresses)
+    print(instance.status)
