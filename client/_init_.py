4201e73c2d93ef08efb2be02f9f67de8ee87e407
Anirudh Srivatsa <arsrivat@Anirudhs-MacBook-Air.local>
2021-11-24 14:25:41 -0500

    Checking for ping

--- client/_init_.py
+++ client/_init_.py
@@ -1,5 +1,7 @@
 import csv
-import novaclient
+# import novaclient
+# from novaclient import client
+import os
 header = ['VM', 'IsAlive', 'Space']
 # import psycopg2
 # from psycopg2.extras import LogicalReplicationConnection
@@ -7,7 +9,11 @@ header = ['VM', 'IsAlive', 'Space']
 with open('central_repository.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
-for instance in novaclient.servers.list():
-    print(instance.name)
-    print(instance.addresses)
-    print(instance.status)
+HOST_UP = False if os.system("ping -c 1 " + "128.31.26.151") != 0 else True
+print(HOST_UP)
+#for instance in client.
+#    print(instance.name)
+#    print(instance.addresses)
+#    print(instance.status)
+
+
