aa7efb51efecf99c9435dad576caf9d872d8c286
Anirudh Srivatsa <arsrivat@Anirudhs-MacBook-Air.local>
2021-11-24 15:31:20 -0500

    csv

--- client/_init_.py
+++ client/_init_.py
@@ -15,6 +15,19 @@ HOST_UP = False if os.system("ping -c 1 " + "10.0.0.125") != 0 else True
 HOST_UP = False if os.system("ping -c 1 " + "192.168.100.66") != 0 else True
 HOST_UP = False if os.system("ping -c 1 " + "10.0.0.17") != 0 else True
 print(HOST_UP)
+writer = csv.DictWriter(f, fieldnames=header)
+for i in range(4):
+    if i == 0:
+        ip = "10.0.0.220"
+    elif i == 1:
+        ip = "10.0.0.125"
+    elif i == 2:
+        ip = "192.168.100.66"
+    elif i == 3:
+        ip = "10.0.0.17"
+    HOST_UP = False if os.system("ping -c 1 " + ip) != 0 else True
+    row = {'ID': i + 1, 'IsAlive': HOST_UP, 'Space': "unknown"}
+    writer.writerow(row)
 #for instance in client.
 #    print(instance.name)
 #    print(instance.addresses)
