7c4ceb979e16bbe2d22331484f8024e186ee8f09
Anirudh Srivatsa <arsrivat@Anirudhs-MacBook-Air.local>
2021-11-24 14:39:28 -0500

    Checking for ping hits

--- client/_init_.py
+++ client/_init_.py
@@ -9,7 +9,12 @@ header = ['VM', 'IsAlive', 'Space']
 with open('central_repository.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
+
+HOST_UP = False if os.system("ping -c 1 " + "10.0.0.220") != 0 else True
+HOST_UP = False if os.system("ping -c 1 " + "10.0.0.125") != 0 else True
+HOST_UP = False if os.system("ping -c 1 " + "192.168.100.66") != 0 else True
 HOST_UP = False if os.system("ping -c 1 " + "10.0.0.17") != 0 else True
+
 print(HOST_UP)
 #for instance in client.
 #    print(instance.name)
