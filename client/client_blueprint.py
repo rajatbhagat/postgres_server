0d88fcba38f8e791c624e5274ddfad38769544ef
rajatbhagat <rajat.bhagat@gmail.com>
2021-12-03 17:50:24 -0500

    Adding flask restx support. Creates a swagger UI for the APIs

--- client/client_blueprint.py
+++ client/client_blueprint.py
@@ -1,33 +1,42 @@
 from flask import Blueprint
+from flask_restx import Api, Resource
 
 client_blueprint = Blueprint("client_blueprint", __name__)
+client_api = Api(client_blueprint)
+
+client_namespcae = client_api.namespace("client");
+
+@client_namespcae.route("/")
+class ClientIndex(Resource):
+    def get(self):
+        return "Default client api call"
 
 
-@client_blueprint.route("/")
-def index():
-    return "Default client api call"
+@client_namespcae.route("/databaseDetails")
+class DatabaseDetails(Resource):
+    def get(self):
+        return "This call will give us the database details"
 
 
-@client_blueprint.route("/databaseDetails")
-def get_database_details():
-    return "This call will give us the database details"
+@client_namespcae.route("/getConnectionString")
+class ConnectionString(Resource):
+    def get(self):
+        return "This call will give us the connection string to connect to a database"
 
 
-@client_blueprint.route("/getConnectionString")
-def get_connection_string(database_name):
-    return "This call will give us the connection string to connect to a database"
+@client_namespcae.route("/getDatabaseInstanceDetails")
+class DatabaseInstanceDetails(Resource):
+    def get_database_instance_details(self):
+        return "This call will get the database instance details"
 
 
-@client_blueprint.route("/getDatabaseInstanceDetails")
-def get_database_instance_details():
-    return "This call will get the database instance details"
+@client_namespcae.route("/getAvailablSpaceInVm")
+class AvailablSpaceInVM(Resource):
+    def get(self):
+        return "This call will hit the central repo and get the details of the VM that has space"
 
 
-@client_blueprint.route("/getAvailablSpaceInVm")
-def get_available_space_in_vm():
-    return "This call will hit the central repo and get the details of the VM that has space"
-
-
-@client_blueprint.route("/updateCentralDatabase")
-def update_central_database():
-    return "This call will update the central database."
+@client_namespcae.route("/updateCentralDatabase")
+class UpdateCentralRepository(Resource):
+    def get(self):
+        return "This call will update the central database."
--- postgres_instance/instance_blueprint.py
+++ postgres_instance/instance_blueprint.py
@@ -1,23 +1,31 @@
 from flask import Blueprint
+from flask_restx import Api, Resource
 
 instance_blueprint = Blueprint("instance_blueprint", __name__)
+instance_api = Api(instance_blueprint)
+
+instance_namespace = instance_api.namespace("instance")
 
 
-@instance_blueprint.route("/")
-def instance_index():
-    return "Default call for the instance apis"
+@instance_namespace.route("/")
+class InstanceBasic(Resource):
+    def get(self):
+        return "Default call for the instance apis"
 
 
-@instance_blueprint.route("/createInstance")
-def create_instance():
-    return "This call will create an instance."
+@instance_namespace.route("/createInstance")
+class CreateInstanceBasic(Resource):
+    def get(self):
+        return "This call will create an instance."
 
 
-@instance_blueprint.route("/getInstanceDetails")
-def get_instance_details():
-    return "This call will get the details of the instance."
+@instance_namespace.route("/getInstanceDetails")
+class InstanceDetails(Resource):
+    def get(self):
+        return "This call will get the details of the instance."
 
 
-@instance_blueprint.route("/modifyInstanceSettings")
-def modify_instance_details():
-    return "This call will modify the instance settings."
+@instance_namespace.route("/modifyInstanceSettings")
+class ModifyInstanceDetails(Resource):
+    def get(self):
+        return "This call will modify the instance settings."
